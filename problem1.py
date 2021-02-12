d9bfbcf72882cda327bad2eb0b18cca19990d26c
Oscar Lin <oscar.lin23@ycdsbk12.ca>
2021-02-12 14:06:11 +0000

    first commit

--- problem1.py
+++ problem1.py
@@ -1,1 +1,18 @@
+"""
+*******************************************************************************
+Filename:       2_livehack_practice_solution1.py
+Description:    Determine if a triangle is a right angle triangle
+Author:         Lin.O
+Created On:     12/02/2021
+*******************************************************************************
+"""
+#get 3 sides for the triangle 
+side1 = int(input("Enter the length of side 1: "))
+side2 = int(input("Enter the length of side 2: "))
+side3 = int(input("Enter the length of side 3: "))
 
+#compute and result 
+if (side1**2) + (side2**2) == side3**2:
+  print("The triangle with side lengths " + str(side1) + ", " + str(side2) + ", " + str(side3) + ", " + str("form a right-angled triangle. "))
+else:
+  print("The triangle with side lengths " + str(side1) + ", " + str(side2) + ", " + str(side3) + ", " + str("does not form a right-angled triangle. "))
--- problem2.py
+++ problem2.py
@@ -1,1 +1,19 @@
+"""
+*******************************************************************************
+Filename:       2_livehack_practice_solution2.py
+Description:    Compute the destination of a student based on marks and earnings
+Author:         Lin.O
+Created On:     12/02/2021
+*******************************************************************************
+"""
+#get mark and earnings before the summer 
+mark = float(input("Mark: "))
+earnings_before_the_summer = float(input("Earnings before the summer: "))
 
+#compute and result 
+if mark >= 80 and earnings_before_the_summer >= 500: 
+  print("You can go to Europe!")
+elif mark >= 80:
+  print("You can go to California!")
+else:
+  print("You cannot go anywhere! ")
